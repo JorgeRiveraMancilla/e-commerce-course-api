22833f7f1f48b251b42c0ea4265354602ed5e7c9
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-08-08 17:29:03 -0400

    docs: add docstring

--- Middleware/ExceptionMiddleware.cs
+++ Middleware/ExceptionMiddleware.cs
@@ -3,18 +3,54 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace e_commerce_course_api.Middleware
 {
+    /// <summary>
+    /// Represents an exception middleware.
+    /// </summary>
+    /// <param name="next">
+    /// The next request delegate.
+    /// </param>
+    /// <param name="logger">
+    /// The logger.
+    /// </param>
+    /// <param name="env">
+    /// The host environment.
+    /// </param>
     public class ExceptionMiddleware(
         RequestDelegate next,
         ILogger<ExceptionMiddleware> logger,
         IHostEnvironment env
     )
     {
+        /// <summary>
+        /// The next request delegate.
+        /// </summary>
         private readonly RequestDelegate _next = next;
+
+        /// <summary>
+        /// The logger.
+        /// </summary>
         private readonly ILogger<ExceptionMiddleware> _logger = logger;
+
+        /// <summary>
+        /// The host environment.
+        /// </summary>
         private readonly IHostEnvironment _env = env;
+
+        /// <summary>
+        /// The options for the JSON serializer.
+        /// </summary>
         private static readonly JsonSerializerOptions _options =
             new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
+        /// <summary>
+        /// Invokes the exception middleware.
+        /// </summary>
+        /// <param name="context">
+        /// The HTTP context.
+        /// </param>
+        /// <returns>
+        /// A task representing the asynchronous operation.
+        /// </returns>
         public async Task InvokeAsync(HttpContext context)
         {
             try
