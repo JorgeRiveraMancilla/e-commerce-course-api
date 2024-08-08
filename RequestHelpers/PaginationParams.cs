7b5ad4931fff8256992e96793539add5e31fb752
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-08-08 18:33:19 -0400

    feat: add parameter class for product requests

---
+++ RequestHelpers/PaginationParams.cs
@@ -0,0 +1,32 @@
+namespace e_commerce_course_api.RequestHelpers
+{
+    /// <summary>
+    /// Represents the parameters for pagination.
+    /// </summary>
+    public class PaginationParams
+    {
+        /// <summary>
+        /// The maximum page size.
+        /// </summary>
+        private const int maxPageSize = 50;
+
+        /// <summary>
+        /// The current page number.
+        /// </summary>
+        public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// The number of items per page.
+        /// </summary>
+        private int _pageSize = 6;
+
+        /// <summary>
+        /// The number of items per page.
+        /// </summary>
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > maxPageSize ? maxPageSize : value;
+        }
+    }
+}
---
+++ RequestHelpers/ProductParams.cs
@@ -0,0 +1,28 @@
+namespace e_commerce_course_api.RequestHelpers
+{
+    /// <summary>
+    /// Represents the parameters for a product request.
+    /// </summary>
+    public class ProductParams : PaginationParams
+    {
+        /// <summary>
+        /// The order by clause.
+        /// </summary>
+        public string? OrderBy { get; set; }
+        
+        /// <summary>
+        /// The search term.
+        /// </summary>
+        public string? SearchTerm { get; set; }
+        
+        /// <summary>
+        /// The types.
+        /// </summary>
+        public string? Types { get; set; }
+        
+        /// <summary>
+        /// The brands.
+        /// </summary>
+        public string? Brands { get; set; }
+    }
+}
