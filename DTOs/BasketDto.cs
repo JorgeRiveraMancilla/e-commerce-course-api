f53fd222bfd09ff4cf51467d846eb0e39024e034
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-08-10 17:56:11 -0400

    docs: add docstring

--- DTOs/BasketDto.cs
+++ DTOs/BasketDto.cs
@@ -1,9 +1,23 @@
 namespace e_commerce_course_api.DTOs
 {
+    /// <summary>
+    /// Data Transfer Object for Basket.
+    /// </summary>
     public class BasketDto
     {
+        /// <summary>
+        /// Id.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// BuyerId.
+        /// </summary>
         public required string BuyerId { get; set; }
+
+        /// <summary>
+        /// Items.
+        /// </summary>
         public required List<BasketItemDto> Items { get; set; }
     }
 }
--- DTOs/BasketItemDto.cs
+++ DTOs/BasketItemDto.cs
@@ -1,13 +1,43 @@
 namespace e_commerce_course_api.DTOs
 {
+    /// <summary>
+    /// Data Transfer Object for BasketItem.
+    /// </summary>
     public class BasketItemDto
     {
+        /// <summary>
+        /// ProductId.
+        /// </summary>
         public int ProductId { get; set; }
+
+        /// <summary>
+        /// Name.
+        /// </summary>
         public required string Name { get; set; }
+
+        /// <summary>
+        /// Price.
+        /// </summary>
         public long Price { get; set; }
+
+        /// <summary>
+        /// URL of the image.
+        /// </summary>
         public required string ImageUrl { get; set; }
+
+        /// <summary>
+        /// Brand.
+        /// </summary>
         public required string Brand { get; set; }
+
+        /// <summary>
+        /// Type.
+        /// </summary>
         public required string Type { get; set; }
+
+        /// <summary>
+        /// Quantity.
+        /// </summary>
         public int Quantity { get; set; }
     }
 }
