2510bcc59a74397b28cd41064e2848c1f138cd1a
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-08-10 16:57:00 -0400

    feat: generate user entity

---
+++ Entities/User.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace e_commerce_course_api.Entities
+{
+    /// <summary>
+    /// Represents a user.
+    /// </summary>
+    public class User : IdentityUser { }
+}
