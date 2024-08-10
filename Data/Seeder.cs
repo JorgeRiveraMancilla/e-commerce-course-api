ab9a9d94370f869a2622de1483654d7d7fd43b57
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-08-10 16:57:31 -0400

    feat: add user seeder

--- Data/Seeder.cs
+++ Data/Seeder.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using e_commerce_course_api.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace e_commerce_course_api.Data
 {
@@ -15,11 +16,32 @@ namespace e_commerce_course_api.Data
             new() { PropertyNameCaseInsensitive = true };
 
         /// <summary>
+        /// Seeds the database.
+        /// </summary>
+        /// <param name="dataContext">
+        /// The data context.
+        /// </param>
+        /// <param name="userManager">
+        /// The user manager.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the asynchronous operation.
+        /// </returns>
+        public static async Task SeedAsync(DataContext dataContext, UserManager<User> userManager)
+        {
+            await SeedProductsAsync(dataContext);
+            await SeedUsersAsync(userManager);
+        }
+
+        /// <summary>
         /// Seeds the products.
         /// </summary>
         /// <param name="dataContext">
         /// The data context.
         /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the asynchronous operation.
+        /// </returns>
         public static async Task SeedProductsAsync(DataContext dataContext)
         {
             if (dataContext.Products.Any())
@@ -33,6 +55,31 @@ namespace e_commerce_course_api.Data
 
             await dataContext.AddRangeAsync(products);
             await dataContext.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Seeds the users.
+        /// </summary>
+        /// <param name="userManager">
+        /// The user manager.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the asynchronous operation.
+        /// </returns>
+        public static async Task SeedUsersAsync(UserManager<User> userManager)
+        {
+            if (userManager.Users.Any())
+                return;
+
+            var user = new User { UserName = "bob", Email = "bob@test.com" };
+
+            await userManager.CreateAsync(user, "Pa$$w0rd");
+            await userManager.AddToRoleAsync(user, "Member");
+
+            var admin = new User { UserName = "admin", Email = "admin@test.com" };
+
+            await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, ["Admin", "Member"]);
         }
     }
 }
