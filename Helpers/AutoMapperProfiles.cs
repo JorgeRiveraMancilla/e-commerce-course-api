20ced287d3fd3dec347406fc2fc410956ced810b
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-08-14 01:56:09 -0400

    feat: set new mappers

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -1,12 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using e_commerce_course_api.DTOs;
+using e_commerce_course_api.Entities;
 
 namespace e_commerce_course_api.Helpers
 {
-    public class AutoMapperProfiles
+    public class AutoMapperProfiles : Profile
     {
-        
+        public AutoMapperProfiles()
+        {
+            CreateMap<Basket, BasketDto>();
+            CreateMap<BasketItem, BasketItemDto>()
+                .ForMember(x => x.ProductId, o => o.MapFrom(s => s.Product.Id))
+                .ForMember(x => x.Name, o => o.MapFrom(s => s.Product.Name))
+                .ForMember(x => x.Price, o => o.MapFrom(s => s.Product.Price))
+                .ForMember(x => x.ImageUrl, o => o.MapFrom(s => s.Product.ImageUrl))
+                .ForMember(x => x.Brand, o => o.MapFrom(s => s.Product.Brand))
+                .ForMember(x => x.Type, o => o.MapFrom(s => s.Product.Type));
+            CreateMap<Product, ProductDto>();
+            CreateMap<User, UserDto>();
+        }
     }
 }
