204635c1fe6458a2e275f5dc42cc2c24ee8864ed
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-08-25 04:35:25 -0400

    fix: origin cors

--- Program.cs
+++ Program.cs
@@ -122,7 +122,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 app.UseCors(x =>
-    x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(Environment.GetEnvironmentVariable("ORIGIN_URL")!)
+    x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(Environment.GetEnvironmentVariable("ORIGIN")!)
 );
 app.UseAuthentication();
 app.UseAuthorization();
