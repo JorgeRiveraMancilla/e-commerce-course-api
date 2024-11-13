3e4ef5a3d8360a61aace67eae9d4829cf13de567
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-12 22:46:58 -0300

    feat: add public id attribute

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -14,35 +14,40 @@ namespace e_commerce_course_api.Entities
         /// The name of the product.
         /// </summary>
         public required string Name { get; set; }
-        
+
         /// <summary>
         /// The description of the product.
         /// </summary>
         public required string Description { get; set; }
-        
+
         /// <summary>
         /// The price of the product.
         /// </summary>
         public required long Price { get; set; }
-        
+
         /// <summary>
         /// The URL of the image of the product.
         /// </summary>
         public required string ImageUrl { get; set; }
-        
+
         /// <summary>
         /// The type of the product.
         /// </summary>
         public required string Type { get; set; }
-        
+
         /// <summary>
         /// The brand of the product.
         /// </summary>
         public required string Brand { get; set; }
-        
+
         /// <summary>
         /// The stock of the product.
         /// </summary>
         public required int Stock { get; set; }
+
+        /// <summary>
+        /// The public identifier of the product.
+        /// </summary>
+        public required string PublicId { get; set; }
     }
 }
