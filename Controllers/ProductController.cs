ad7f56370e5e2e2d85c4ab71bfb8bd86b6385821
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-11-12 22:47:26 -0300

    feat: add create, update and delete endpoints

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,7 +1,8 @@
-using e_commerce_course_api.DTOs;
+using e_commerce_course_api.DTOs.Products;
 using e_commerce_course_api.Extensions;
 using e_commerce_course_api.Helpers.Requests;
 using e_commerce_course_api.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace e_commerce_course_api.Controllers
@@ -48,7 +49,7 @@ namespace e_commerce_course_api.Controllers
         /// <returns>
         /// The product if exists, otherwise not found.
         /// </returns>
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetProduct")]
         public async Task<ActionResult<ProductDto>> GetProduct(int id)
         {
             var product = await _productRepository.GetProductByIdAsync(id);
@@ -72,6 +73,83 @@ namespace e_commerce_course_api.Controllers
             var types = await _productRepository.GetProductTypesAsync();
 
             return Ok(new { brands, types });
+        }
+
+        /// <summary>
+        /// Create a product.
+        /// </summary>
+        /// <param name="createProductDto">
+        /// The product to create.
+        /// </param>
+        /// <returns>
+        /// The product created.
+        /// </returns>
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> CreateProduct(
+            [FromForm] CreateProductDto createProductDto
+        )
+        {
+            try
+            {
+                var product = await _productRepository.CreateProductAsync(createProductDto);
+
+                if (await _productRepository.SaveChangesAsync())
+                    return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ProblemDetails { Title = e.Message });
+            }
+
+            return BadRequest(new ProblemDetails { Title = "Error al crear el producto" });
+        }
+
+        /// <summary>
+        /// Update a product.
+        /// </summary>
+        /// <param name="productDto">
+        /// The product to update.
+        /// </param>
+        /// <returns>
+        /// The product updated.
+        /// </returns>
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public async Task<ActionResult<ProductDto>> UpdateProduct(
+            [FromForm] UpdateProductDto productDto
+        )
+        {
+            var exists = await _productRepository.ProductExistsByIdAsync(productDto.Id);
+
+            if (!exists)
+                return NotFound();
+
+            try
+            {
+                var product = await _productRepository.UpdateProductAsync(productDto);
+
+                if (await _productRepository.SaveChangesAsync())
+                    return Ok(product);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ProblemDetails { Title = e.Message });
+            }
+
+            return BadRequest(new ProblemDetails { Title = "Error al actualizar el producto" });
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            await _productRepository.DeleteProductByIdAsync(id);
+
+            if (await _productRepository.SaveChangesAsync())
+                return Ok();
+
+            return BadRequest(new ProblemDetails { Title = "Error al eliminar el producto" });
         }
     }
 }
